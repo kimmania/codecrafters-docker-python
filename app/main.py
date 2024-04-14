004a18e9b5317dd12d58046f4777e33c606b4ec3
Kim Mann <kimmania@gmail.com>
2024-04-14 11:46:13 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
+'''Main worker'''
 import subprocess
 import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
+    '''Main execution'''
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    completed_process = subprocess.run([command, *args], check=False, capture_output=True)
+    sys.stdout.buffer.write(completed_process.stdout)
+    sys.stderr.buffer.write(completed_process.stderr)
 
 
 if __name__ == "__main__":

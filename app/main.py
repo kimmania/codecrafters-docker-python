70973ab7f9eaed3ca9fa32cf4740ba0f1202715e
Kim Mann <kimmania@gmail.com>
2024-04-14 12:12:40 -0400

    stage

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ def main():
     '''Main execution'''
     command = sys.argv[3]
     args = sys.argv[4:]
-    
+
     completed_process = subprocess.run([command, *args], check=False, capture_output=True)
     sys.stdout.buffer.write(completed_process.stdout)
     sys.stderr.buffer.write(completed_process.stderr)
-
+    sys.exit(completed_process.returncode)
 
 if __name__ == "__main__":
     main()
